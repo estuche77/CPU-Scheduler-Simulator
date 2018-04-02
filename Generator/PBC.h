f33ac70adf51d7766726b9c64a9a7fcb7adebb85
Jason Latouche <96jaslat@gmail.com>
2018-04-02 14:57:36 -0600

    Error in creating files for projects - checkpoint

--- Generator/PBC.h
+++ Generator/PBC.h
@@ -5,4 +5,15 @@
 #ifndef GENERATOR_PBC_H
 #define GENERATOR_PBC_H
 
+enum pState{Ready, Active, Ended};
+
+typedef struct PCB {
+    int pid;
+    int burst;
+    int priority;
+    enum pState state;
+    int tat;
+    int wt;
+};
+
 #endif //GENERATOR_PBC_H
