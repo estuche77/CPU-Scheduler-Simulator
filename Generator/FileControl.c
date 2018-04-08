65ade173a8d45ed0d64b89f70b44fb1f04d5ba8b
matt96 <jkherrera96@gmail.com>
2018-04-07 20:13:23 -0600

    filecontrols was added

---
+++ Generator/FileControl.c
@@ -0,0 +1,5 @@
+//
+// Created by shake on 07/04/18.
+//
+
+#include "FileControl.h"
--- Generator/main.c
+++ Generator/main.c
@@ -5,7 +5,12 @@
 #include "Process.h"
 #include "Socket.h"
 #include "Utils.h"
+#include <unistd.h>
 
+#define LOWERBURST 3
+#define HIGHBURST 8
+#define LOWERPRIORITY 5
+#define HIGHPRIORITY 1
 #define PORT 8080
 #define HOST "127.0.0.1"
 
@@ -15,11 +20,11 @@ int main() {
     srand((unsigned int) time(NULL));
 
     // Randomly create 300 process and send it to server
-    for (int i = 0; i < 500; i++) {
+    for (int i = 0; i < 5; i++) {
 
         // Random values
-        int randomBurst = randIntBetween(0, 100);
-        int randomPriority = randIntBetween(1, 20);
+        int randomBurst = randIntBetween(LOWERBURST, HIGHBURST);
+        int randomPriority = randIntBetween(HIGHPRIORITY, LOWERPRIORITY);
 
         // Process creation
         Process *process = newProcess(randomBurst, randomPriority);
@@ -28,6 +33,7 @@ int main() {
         // Connection and communication
         int socket = connectSocket(HOST, PORT);
         startCommunication(socket, str);
+        sleep(1);
     }
 
     return 0;
