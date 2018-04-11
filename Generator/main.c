9a31c0c5bf94e3b835603266874704d784304c96
matt96 <jkherrera96@gmail.com>
2018-04-10 22:45:11 -0600

    get path file

--- Generator/main.c
+++ Generator/main.c
@@ -8,7 +8,7 @@
 #include "Socket.h"
 #include "Utils.h"
 #include "MessageControl.h"
-
+#include <string.h>
 void setAddress(Message *m);
 void setFilePath(Message *m);
 void setDataMessages(Message *m);
@@ -74,6 +74,7 @@ void setAddress(Message *m) {
 
 void setFilePath(Message *m) {
     int option = 0;
+    char* p=(char*)malloc(64);
     do {
         printf("\n************Manual****************\n");
         printf("*                                *\n");
@@ -87,8 +88,11 @@ void setFilePath(Message *m) {
         while (getchar() != '\n');
         switch (option) {
             case 1:
+
                 printf("* --> File path: ");
-                fgets(m->directory, 64, stdin);
+                fgets(p, 64, stdin);
+                strcpy(p, strtok(p, "\n"));
+                m->directory=p;
                 break;
             case 2:
                 break;
